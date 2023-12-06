8f03f546f3e72d208fdbbf2060b7f05208e62f71
cqueiroga <chris.queiroga@gmail.com>
2023-12-06 15:01:46 -0500

    - Added CSS file - Created 16x16 grid

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+const container = document.getElementById("container");
+const grid = document.createElement("div");
 
+// Create grid = 52 x 45
+for (let i = 0; i < 2340; i++) {
+	const grid = document.createElement("div");
+	container.append(grid);
+	grid.classList.add("grids");
+}
