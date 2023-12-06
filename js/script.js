f6ee5503b9374d2bfe79af79cf034c20d66486c2
cqueiroga <chris.queiroga@gmail.com>
2023-12-06 15:26:30 -0500

    - Added function to change background style to red on mouseover

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ for (let i = 0; i < 2340; i++) {
 	container.append(grid);
 	grid.classList.add("grids");
 }
+
+container.onmouseover = handler;
+
+// mouseover event function
+// on mouseover, change background style to red
+function handler(event) {
+	if (event.x < 832 && event.x > 0 && event.y > 0 && event.y < 720) {
+		if (event.type == "mouseover") {
+			event.target.style.background = "red";
+		}
+	}
+}
